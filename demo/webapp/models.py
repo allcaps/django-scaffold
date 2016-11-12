ace8c25963c2bc2f024f1ca5c8101f916ffc2104
Coen van der Kamp <coen@fourdigits.nl>
2016-11-13 00:03:21 +0100

    Fix demo app starting point

--- demo/webapp/models.py
+++ demo/webapp/models.py
@@ -1,5 +1,29 @@
+# -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=300)
+    num_awards = models.IntegerField()
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=300)
+    pages = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    rating = models.FloatField()
+    authors = models.ManyToManyField(Author)
+    publisher = models.ForeignKey(Publisher)
+    pubdate = models.DateField()
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book)
+    registered_users = models.PositiveIntegerField()
